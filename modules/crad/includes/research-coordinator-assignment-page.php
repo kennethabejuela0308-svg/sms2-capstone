fc622f8e1f2a711c82c2e673de9fe2a13caad63c
Kenneth Abejuela <kennethabejuela0308@gmail.com>
2026-09-18 23:03:58 +0800

    Auto-sync: 2026-09-18 23:03:58

--- modules/crad/includes/research-coordinator-assignment-page.php
+++ modules/crad/includes/research-coordinator-assignment-page.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/../config/config.php';
 require_once ROOT_PATH . '/includes/authentication.php';
 require_once ROOT_PATH . '/includes/breadcrumbs.php';
 require_once ROOT_PATH . '/includes/notifications.php';
+require_once __DIR__ . '/title-approval-assignees.php';
 
 requireAuth();
 
