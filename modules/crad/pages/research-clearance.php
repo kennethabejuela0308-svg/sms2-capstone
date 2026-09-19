727c47bd58751b99026c207d97b6f9c31a755a8c
Kenneth Abejuela <kennethabejuela0308@gmail.com>
2026-09-19 11:13:12 +0800

    Auto-sync: 2026-09-19 11:13:12

--- modules/crad/pages/research-clearance.php
+++ modules/crad/pages/research-clearance.php
@@ -32,7 +32,7 @@ $rscSigPadLabel = 'CRAD Signature Pad (Draw Below)';
 require_once ROOT_PATH . '/includes/layout-start.php';
 renderBreadcrumbs($breadcrumbs);
 ?>
-<link rel="stylesheet" href="<?= BASE_URL ?>/modules/crad/assets/css/research-clearance.css?v=rsc-reupload-7">
+<link rel="stylesheet" href="<?= BASE_URL ?>/modules/crad/assets/css/research-clearance.css?v=rsc-size-1">
 
 <div class="glass-dashboard rsc-print-root"
      data-rsc-live
