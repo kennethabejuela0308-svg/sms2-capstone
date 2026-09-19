83dc55781f81cc50b03fba29a6a33540fd000b1a
Kenneth Abejuela <kennethabejuela0308@gmail.com>
2026-09-19 11:19:39 +0800

    Auto-sync: 2026-09-19 11:19:39

--- modules/crad/includes/research-services-clearance.php
+++ modules/crad/includes/research-services-clearance.php
@@ -1111,6 +1111,8 @@ function rscRenderFormHtml(array $row, bool $duplicate = true): string
         $aaImg = $aaSig !== '' ? '<img src="' . $e($aaSig) . '" alt="AA signature">' : '';
         $cradImg = $cradSig !== '' ? '<img src="' . $e($cradSig) . '" alt="CRAD signature">' : '';
         $adviserDate = rscFormatDate($row['adviser_signed_at'] ?? null);
+        $misDate = rscFormatDate($row['mis_verified_at'] ?? $row['uploaded_at'] ?? null);
+        $aaDate = rscFormatDate($row['aa_verified_at'] ?? $row['uploaded_at'] ?? null);
         $cradDate = rscFormatDate($row['crad_signed_at'] ?? null);
 
         return '<div class="rsc-sheet">'
